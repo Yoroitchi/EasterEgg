1200f0a46d5ba1ebbdbbbfbbcc92c751336d57eb
WhatTheSlime <selim.lanouar@gmail.com>
2017-04-22 21:56:23 +0200

    Start Of Project

--- EasterEgg/src/main/java/easteregg/Kid.java
+++ EasterEgg/src/main/java/easteregg/Kid.java
@@ -4,12 +4,17 @@ package easteregg;
  * Created by Selim on 22/04/2017.
  */
 public class Kid extends Element{
+
     String sequence;
     public Egg[] sac;
 
-    public Kid(String sequence, Egg[] sac) {
+    public Kid() {
+    }
+
+    public Kid(String sequence, int posX, int posY) {
         this.sequence = sequence;
-        this.sac = sac;
+        this.posX = posX;
+        this.posY = posY;
     }
 
     public String getSequence() {
