31c8e21bf31b79c0aabe5500fe440058c38cd2a8
WhatTheSlime <selim.lanouar@gmail.com>
2017-04-22 19:43:13 +0200

    Revert "Restar Project"
    This reverts commit 994ea2308cc8f1777fcc9b6456f2bfcede28eebe.

---
+++ src/main/java/backend/GameManager.java
@@ -0,0 +1,22 @@
+package org.easteregg;
+
+/**
+ * Created by r1leg on 22/04/2017.
+ */
+public class GameManager {
+   public float waitTime;
+
+    //region Function
+    public void StartGame() {
+
+    }
+
+    public void EndGame() {
+
+    }
+    //endregion
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/dao/GardenDao.java
@@ -0,0 +1,7 @@
+package main.java.dao;
+
+/**
+ * Created by Noddin on 22/04/2017.
+ */
+public class GardenDao {
+}
