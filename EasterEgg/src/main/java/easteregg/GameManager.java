b0b48202c630c14a5ca54d9e06a844787b5b8c14
WhatTheSlime <selim.lanouar@gmail.com>
2017-04-23 18:51:48 +0200

    All DAO
    All DAO are define, Initalisation of the garden txt files

--- EasterEgg/src/main/java/easteregg/GameManager.java
+++ EasterEgg/src/main/java/easteregg/GameManager.java
@@ -1,16 +1,33 @@
 package easteregg;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Selim on 22/04/2017.
  */
 public class GameManager {
 
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        Garden garden = new Garden(4, 6);
-        System.out.println(" "+garden.sizeX +" "+garden.sizeY);
-        garden.printTable();
-        KidDao kidDao = new KidDao();
-        kidDao.findAllKids();
+    public final Garden garden = new GardenDao().findGarden();
+    public final ArrayList<Kid> kids = new KidDao().findAllKids();
+    public final ArrayList<EggStack> eggStacks = new EggStackDao().findAllEggs();
+    public final ArrayList<Rock> rocks = new RockDao().findAllRocks();
+
+    public void instanceGame(){
+        Iterator<Kid> itKid = kids.iterator();
+        Iterator<EggStack> itEggStack = eggStacks.iterator();
+        Iterator<Rock> itRock = rocks.iterator();
+
+        while(itKid.hasNext()){
+            garden.addKid(itKid.next());
+        }
+
+        while(itEggStack.hasNext()){
+            garden.addEgg(itEggStack.next());
+        }
+
+        while(itRock.hasNext()){
+            garden.addRock(itRock.next());
+        }
     }
 }
--- EasterEgg/src/main/java/easteregg/GardenDao.java
+++ EasterEgg/src/main/java/easteregg/GardenDao.java
@@ -10,5 +10,32 @@ import java.util.List;
  * Created by Selim on 22/04/2017.
  */
 public class GardenDao {
+    public Garden findGarden() {
+        Garden garden = null;
 
+        try {
+            InputStreamReader fr = new InputStreamReader(new FileInputStream("src/main/resources/garden.txt"));
+            BufferedReader br = new BufferedReader(fr);
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                if (ligne.startsWith("J")) {
+                    System.out.println(ligne);
+                    garden = transformLigneToGarden(ligne);
+                }
+            }
+            br.close();
+            fr.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return garden;
+    }
+
+    private Garden transformLigneToGarden(final String ligne) throws Exception {
+
+        String[] values = ligne.split(" ");
+        Garden garden = new Garden (new Integer(values[1]), new Integer(values[2]));
+
+        return garden;
+    }
 }
