296b5dd6de5b3c64324247be787f14e51bdf4b37
WhatTheSlime <selim.lanouar@gmail.com>
2017-04-25 01:05:40 +0200

    Empty Tests
    Generation of all test we need for now

---
+++ EasterEgg/src/test/java/dao/EggStackDaoTest.java
@@ -0,0 +1,25 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Selim on 25/04/2017.
+ */
+public class EggStackDaoTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAllEggs() throws Exception {
+    }
+
+}
